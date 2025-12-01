f91154607d4592a54f6c960fbd3315a66fa7dfb8
AlessandroTinaoui <tinaouialessandro02@gmail.com>
2025-12-01 22:16:36 +0100

    fixed linesearch

--- src/LineSearch.cpp
+++ src/LineSearch.cpp
@@ -1,29 +1,23 @@
-
 #include "LineSearch.hpp"
-#include <Eigen/Dense>
-#include <memory>
-#include <stdexcept>
-#include <string>
 
-#include "Functor.hpp"
-#include <cmath>
+#define MAX_ITER 100
 
 namespace lfbgs {
 
 class BacktrackingArmijo : public LineSearch {
 public:
-    double compute(const Functor& obj, const Eigen::VectorXd& x, const Eigen::VectorXd& g, const Eigen::VectorXd& p) override {
+    double compute(const std::function<double(const Eigen::VectorXd&)>& fun, const Eigen::VectorXd& x, const Eigen::VectorXd& g, const Eigen::VectorXd& p) override {
         double alpha = 1.0;
         const double c1 = 1e-4;
         const double rho = 0.5;
         
-        double fx = obj(x);
+        double fx = fun(x);
         double gtp = g.dot(p);
         
 
-        int max_iter = 100;
+        int max_iter = MAX_ITER;
         for (int i = 0; i < max_iter; ++i) {
-            double f_new = obj(x + alpha * p);
+            double f_new = fun(x + alpha * p);
             if (f_new <= fx + c1 * alpha * gtp) {
                 return alpha;
             }
@@ -35,26 +29,26 @@ public:
 
 class StrongWolfe : public LineSearch {
 public:
-    double compute(const Functor& obj, const Eigen::VectorXd& x, const Eigen::VectorXd& g, const Eigen::VectorXd& p) override {
+    double compute(const std::function<double(const Eigen::VectorXd&)>& fun, const Eigen::VectorXd& x, const Eigen::VectorXd& g, const Eigen::VectorXd& p) override {
         double alpha = 1.0;
         double alpha_prev = 0.0;
         const double c1 = 1e-4;
         const double c2 = 0.9;
         const double alpha_max = 10.0;
         
-        double phi0 = obj(x);
+        double phi0 = fun(x);
         double dphi0 = g.dot(p);
         double phi_prev = phi0;
 
-        int max_iter = 20;
-        for (int i = 1; i <= max_iter; ++i) {
-            double phi = obj(x + alpha * p);
+        for (int i = 1; i <= MAX_ITER; ++i) {
+            double phi = fun(x + alpha * p);
             
+            //armijo e monotonia
             if (phi > phi0 + c1 * alpha * dphi0 || (i > 1 && phi >= phi_prev)) {
-                return zoom(obj, x, p, alpha_prev, alpha, phi0, dphi0, c1, c2);
+                return zoom(fun, x, p, alpha_prev, alpha, phi0, dphi0, c1, c2);
             }
             
-            Eigen::VectorXd g_new = obj.gradient(x + alpha * p);
+            Eigen::VectorXd g_new = gradient(fun, x + alpha * p);
             double dphi = g_new.dot(p);
             
             if (std::abs(dphi) <= -c2 * dphi0) {
@@ -62,7 +56,7 @@ public:
             }
             
             if (dphi >= 0) {
-                return zoom(obj, x, p, alpha, alpha_prev, phi0, dphi0, c1, c2);
+                return zoom(fun, x, p, alpha, alpha_prev, phi0, dphi0, c1, c2);
             }
             
             alpha_prev = alpha;
@@ -73,23 +67,24 @@ public:
     }
 
 private:
-    double zoom(const Functor& obj, const Eigen::VectorXd& x, const Eigen::VectorXd& p, 
+    double zoom(const std::function<double(const Eigen::VectorXd&)>& fun, const Eigen::VectorXd& x, const Eigen::VectorXd& p, 
                 double alpha_lo, double alpha_hi, 
                 double phi0, double dphi0, double c1, double c2) {
         
-        for (int i = 0; i < 20; ++i) {
+        for (int i = 0; i < MAX_ITER; ++i) {
            
            double alpha_j = 0.5 * (alpha_lo + alpha_hi);
            
-           double phi_j = obj(x + alpha_j * p);
-           double phi_lo = obj(x + alpha_lo * p);
+           double phi_j = fun(x + alpha_j * p);
+           double phi_lo = fun(x + alpha_lo * p);
            
+           //armijo e monotonia
            if (phi_j > phi0 + c1 * alpha_j * dphi0 || phi_j >= phi_lo) {
                alpha_hi = alpha_j;
            } else {
-               Eigen::VectorXd g_j = obj.gradient(x + alpha_j * p);
-               double dphi_j = g_j.dot(p);
+               double dphi_j = gradient(fun, x + alpha_j * p).dot(p);
                
+               //curvatura
                if (std::abs(dphi_j) <= -c2 * dphi0) {
                    return alpha_j;
                }
@@ -106,7 +101,6 @@ private:
 
 std::unique_ptr<LineSearch> make_line_search(const std::string& s) {
     if (s == "armijo") return std::make_unique<BacktrackingArmijo>();
-    if (s == "wolfe")  return std::make_unique<StrongWolfe>();
-    throw std::runtime_error("unknown LS");
+    else return std::make_unique<StrongWolfe>(); //default strong wolfe
 }
 } 
--- src/LineSearch.hpp
+++ src/LineSearch.hpp
@@ -1,18 +1,19 @@
 
 #pragma once
-#include <Eigen/Dense>
-
 #include <memory>
 #include <string>
+#include <cmath>
+#include <functional>
+#include <Eigen/Dense>
+#include "MathTools.hpp"
 
 namespace lfbgs {
 
-class Objective;
 
 class LineSearch {
 public:
     virtual ~LineSearch() = default;
-    virtual double compute(const Objective&, const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&) = 0;
+    virtual double compute(const std::function<double(const Eigen::VectorXd&)>& fun, const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&) = 0;
 };
 
 std::unique_ptr<LineSearch> make_line_search(const std::string& s);
