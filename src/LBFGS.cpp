71695446bdd7ff91871521f8ca1dad4f4ce8285f
Davide Cutrupi <davidecutrupi2@gmail.com>
2025-12-05 08:18:52 +0000

    Change indentation

--- src/LBFGS.cpp
+++ src/LBFGS.cpp
@@ -24,30 +24,30 @@ void LBFGS<m>::run() {
   history.reserve(m);
   alpha.reserve(m);
 
-    int iter = 0;
-    VectorXd x = x0_;
-    VectorXd grad = MathTools::gradient(fun_, x);
-    VectorXd d, s, y, q;
+  int iter = 0;
+  VectorXd x = x0_;
+  VectorXd grad = MathTools::gradient(fun_, x);
+  VectorXd d, s, y, q;
   
-    while(grad.norm() > tol_) {
+  while(grad.norm() > tol_) {
 
-      if (iter < m)
-        d = BFGS::computeDirectionP(B, grad);
-      else {
-        q = grad;
-        q = backward_pass(q);
+    if (iter < m)
+      d = BFGS::computeDirectionP(B, grad);
+    else {
+      q = grad;
+      q = backward_pass(q);
       VectorXd r = B*q; // to do
-        d = -forward_pass(r);
-      }
+      d = -forward_pass(r);
+    }
 
     BFGS::updateSolution(x, grad, d, s, y);
 
-        if(history.size()==m)
-          history.emplace_back(history.begin());
+    if(history.size()==m)
+      history.emplace_back(history.begin());
     
-        history.push_back(s, y);  
+    history.push_back(s, y);
 
-        ++iter;
-    }     
-      
+    ++iter;
+  }     
+    
 }
