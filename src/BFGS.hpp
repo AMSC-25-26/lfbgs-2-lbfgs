575c12dc63f99a6ba10bca2b41caac98be1d03c0
Davide Cutrupi <davidecutrupi2@gmail.com>
2025-12-05 09:02:13 +0000

    Remove parameter names in header file and unused parameters

--- src/BFGS.hpp
+++ src/BFGS.hpp
@@ -10,35 +10,31 @@ public:
     // Constructor
     BFGS(const Eigen::VectorXd& x0, const std::function<double(VectorXd const&)>& fun, const double & tol);
 
-    // Update Hessian approximation:
-    // Inputs:
-    //   B_old : previous aproximation
-    //   y : difference between grad(x_k+1) and grad(x_k)
-    //   s: difference between x_k+1 and x_k
-    //Output:
-    //   B_new : current aproximation
-    Eigen::MatrixXd updateB( const Eigen::MatrixXd& B_old,
-                            const Eigen::VectorXd& y,
-                            const Eigen::VectorXd& s);
+      // Update Hessian approximation:
+      // Inputs:
+      //   gamma : difference between grad(x_k+1) and grad(x_k)
+      //   delta: difference between x_k+1 and x_k
+      //Output:
+      //   B_new : current aproximation
+      void updateB(const VectorXd&, const VectorXd&);
 
-    //Update the direction p_k:
-    //Inputs:
-    //   B_old : previous aproximation 
-    //   grad  : gradient at x_k
-    //Output:
-    //   p_new : current direction p_k
-    Eigen::VectorXd computeDirectionP( const  Eigen::MatrixXd& B_old,
-                                       const Eigen::VectorXd& grad);
-    
-    void updateSolution(VectorXd& x_old, VectorXd& grad_old, const VectorXd& d, VectorXd& s, VectorXd& y);
-    virtual void run();
+      //Update the direction p_k:
+      //Inputs:
+      //   grad  : gradient at x_k
+      //Output:
+      //   p_new : current direction p_k
+      VectorXd computeDirectionP(const VectorXd&);
+      
+      void updateSolution(VectorXd&, VectorXd&, const VectorXd&, VectorXd&, VectorXd&);
+      virtual void run();
 
 
-private:
-    std::function<double(VectorXd const&)> fun_; //Function
-    Eigen::VectorXd x0_;   //Initial condition
-    Eigen::MatrixXd B; //Hessian Aproximation
-    double tol_;
+  private:
+      std::function<double(VectorXd const&)> fun_; //Function
+      VectorXd x0_;   //Initial condition
+      MatrixXd B; //Hessian Aproximation
+
+      double tol_;
 };
 
 #endif
