2164ad689c63c7aeba243f448bd0c9783df7b5b9
ddt003 <davide.ditanna@mail.polimi.it>
2025-12-11 01:11:06 +0100

    Implementation of Simple Tests for BFGS and LBFGS

---
+++ test/lbfgs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "BFGS.hpp"
+#include "LBFGS.hpp"
+
+#include "LineSearch.hpp"
+
+#include <Eigen/Dense>
+#include <cmath>
+
+#include "MathTools.hpp"
+
+using PointType = Eigen::VectorXd;
+
+void test_with_lambda_function() {
+    auto myFunc = [](const Eigen::VectorXd &x) -> double {
+        return std::pow(x[0] - 2.0, 2) + std::pow(x[1] + 3.0, 2);
+    };
+
+    Eigen::VectorXd x0(2);
+    x0 << 0.0, 0.0;
+    
+    double tol = 1e-6;
+
+    LBFGS<30> optimizer(x0, myFunc, tol, lfbgs::LineSearchType::StrongWolfe);
+
+    optimizer.run();
+
+    std::cout << "Minimo trovato in: " << optimizer.getCurrentX().transpose() << std::endl;
+    std::cout << "Valore funzione: " << myFunc(optimizer.getCurrentX()) << std::endl;
+
+}
+
+
+
+int main() {
+  test_with_lambda_function();
+
+  return 0;
+}
